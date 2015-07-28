205b0287a583c5c1d7413dfd9feeb334207a5ec3
Jeff Graham <jeff@funnymonkey.com>
2015-07-28 12:04:36 -0700

    Update tests

--- html2text_test.go
+++ html2text_test.go
@@ -1,7 +1,6 @@
 package html2text
 
 import (
-	"regexp"
 	"testing"
 )
 
@@ -14,13 +13,13 @@ func TestText(t *testing.T) {
 			`<li>
   <a href="/new" data-ga-click="Header, create new repository, icon:repo"><span class="octicon octicon-repo"></span> New repository</a>
 </li>`,
-			`/new`,
+			"*   (http://www.microshwhat.com/bar/soapy/new)[New repository]",
 		},
 		{
 			`hi
 
 			<br>
-	
+
 	hello <a href="https://google.com">google</a>
 	<br><br>
 	test<p>List:</p>
@@ -31,18 +30,7 @@ func TestText(t *testing.T) {
         <li>Baz</li>
 	</ul>
 `,
-			`hi
-
-hello
-https://google.com
-test
-
-List:
-
-foo
-http://www.microshwhat.com/bar/soapy
-
-Baz`,
+			"hi \n\n hello  (https://google.com)[google] \n\n test\n\nList: \n\n    *  (foo)[Foo] \n    *  (http://www.microshwhat.com/bar/soapy)[Barsoap] \n    * Baz",
 		},
 		// Malformed input html.
 		{
@@ -59,26 +47,18 @@ Baz`,
 		        <li>Baz</li>
 			</ul>
 		`,
-			`hi hello
-https://google.com
-test
-
-List:
-
-foo
-/\n[ \t]+bar/baz
-
-Baz`,
+			"hi hello  (https://google.com)[google] test\n\nList: \n\n    *  (foo)[Foo] \n    *  (http://www.microshwhat.com/bar/soapy/bar/baz)[Bar] \n    * Baz",
 		},
 	}
 
 	for _, testCase := range testCases {
-		text, err := FromString(testCase.input)
+		text, err := FromString(testCase.input, "http://www.microshwhat.com/bar/soapy", OmitClasses|OmitIds|OmitRoles)
 		if err != nil {
 			t.Error(err)
 		}
-		if expr := regexp.MustCompile(testCase.expr); !expr.MatchString(text) {
-			t.Errorf("Input did not match expression\nInput:\n>>>>\n%s\n<<<<\n\nOutput:\n>>>>\n%s\n<<<<\n\nExpression: %s\n", testCase.input, text, expr.String())
+
+		if testCase.expr != text {
+			t.Errorf("Input did not match expression\nInput:\n>>>>\n@@%s@@\n<<<<\n\nOutput:\n>>>>\n##%s##\n<<<<\n\nExpression: ^^%s^^\n", testCase.input, text, testCase.expr)
 		} else {
 			t.Logf("input:\n\n%s\n\n\n\noutput:\n\n%s\n", testCase.input, text)
 		}
